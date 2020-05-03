b8cba3d805e728b7252bd398a8944a08a6374865
Luis Andres Villegas <luis.villegas@unisimon.edu.co>
2020-05-03 14:53:29 -0500

    operaciones con los numeros

---
+++ src/number.py
@@ -0,0 +1,12 @@
+# Para elevar 
+print(2**3) # out: 8
+
+# Modulo -> residuo de la division
+print(10 % 2)
+
+# Obtener datos por consula input()
+
+age = input('insert your name: ')
+print(type(age))  # para pasar el string a number de la variable age, hacemos esto:
+newAge = int(age) + 5
+print(newAge)

984c31fcdc2c30d39c0d310698bd1343fd518789
Luis Andres Villegas <luis.villegas@unisimon.edu.co>
2020-05-04 23:37:19 -0500

    tuples

---
+++ src/tuples.py
@@ -0,0 +1,19 @@
+x = (1, 2, 3)
+y = tuple((1, 2, 3))
+print(x)
+print(y)
+
+print(dir(x))
+
+# Tupla de un solo elemento
+z = (1,)
+print(z)
+
+# x[1] = 2 Error. 
+
+# Las tuplas pueden ser usadas asi:
+
+locations = {
+    (35.123, 39.000): 'Tokyo',
+    (31.123, 32.000): 'EE.UU'
+}

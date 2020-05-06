ce4caf5dff64444c731ea8865b5f4cc19864e8e9
Luis Andres Villegas <luis.villegas@unisimon.edu.co>
2020-05-06 00:17:07 -0500

    conditionals

---
+++ src/conditionals.py
@@ -0,0 +1,27 @@
+x = 20
+y = 30
+
+# if else
+if x < 30:
+    print('x is less than 30')
+else:
+    print('x es greater than 30')
+
+# elif
+color = 'blue'
+if color == 'red':
+    print('the color is red')
+elif color == 'blue':
+    print('the color is blue')
+else:
+    print('any color')
+
+# Operadores logicos
+if x > 10 and x <= 30:
+    print('x is greater than 10 and less than or equeal 30') 
+
+if x > 10 or x < 30:
+    print('x is greater than 10 or less than 30')
+
+if not x == y: # if not(x == y)
+    print('x no is equal to y')
